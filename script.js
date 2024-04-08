63ae3b700a0684e43b8f518d638ae30b83c34dcc
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-04-08 14:56:28 +0100

    nothing usefull

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").click(function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
-	});
+ 	// $("#stream1_btn").click(function() {
+ 	// 	$(".stream1").removeClass('highlight_stream');
+	// 	$(".stream2").removeClass('highlight_stream');
+	// 	$(".stream3").removeClass('highlight_stream');
+	//   	$(".stream1").addClass('highlight_stream');
+	// });
+	$("#stream1_btn").on(function () {
+		$(".stream1").hide();
+		$(".stream1").hide("slow");
+		$(".stream1").hide("medium");
+		$(".stream1").hide("fast");
+		$(".stream1").hide(1000);
+  	});
+
 	$("#stream2_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
